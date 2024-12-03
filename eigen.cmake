a4bc404daffef669caacd9cdcee6981889f481d6
Gabriele Masina <gabriele.masina@virgilio.it>
2024-12-03 20:28:43 +0100

    Fix compilation dev warning by setting DOWNLOAD_EXTRACT_TIMESTAMP

--- eigen.cmake
+++ eigen.cmake
@@ -2,15 +2,17 @@ include(ExternalProject)
 
 set(EIGEN_INSTALL_DIR "${THIRD_PARTY_DIR}/eigen/")
 
+cmake_policy(SET CMP0135 NEW)
 externalproject_add(
-    eigen
-    URL  https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
-    SOURCE_DIR "${THIRD_PARTY_DIR}/eigen-src"
-    BINARY_DIR "${THIRD_PARTY_DIR}/eigen-build"
-    INSTALL_DIR "${EIGEN_INSTALL_DIR}"
-    CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-    -DCMAKE_BUILD_TYPE=Release
+        eigen
+        URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
+        SOURCE_DIR "${THIRD_PARTY_DIR}/eigen-src"
+        BINARY_DIR "${THIRD_PARTY_DIR}/eigen-build"
+        INSTALL_DIR "${EIGEN_INSTALL_DIR}"
+        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+        CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+        -DCMAKE_BUILD_TYPE=Release
 )
 
 file(MAKE_DIRECTORY "${EIGEN_INSTALL_DIR}/include")  # avoid race condition
@@ -20,6 +22,6 @@ add_dependencies(eigenlib eigen)
 target_compile_features(eigenlib INTERFACE cxx_std_17)
 
 set_target_properties(eigenlib PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES ${EIGEN_INSTALL_DIR}/include/eigen3
+        INTERFACE_INCLUDE_DIRECTORIES ${EIGEN_INSTALL_DIR}/include/eigen3
 )
 
