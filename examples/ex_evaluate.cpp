2186f291364ecd9ce5bc816b64f8a4a29afbdd9b
Gabriele Masina <gabriele.masina@virgilio.it>
2024-12-13 08:50:24 +0100

    Restore ex_evaluate.cpp

--- examples/ex_evaluate.cpp
+++ examples/ex_evaluate.cpp
@@ -4,19 +4,16 @@
 int main()
 {
   // initialize a cubic, non-uniform, open B-spline curve with given knots and control points
-  // constexpr std::size_t degree{3};
-  // const std::vector<double> knots{0.1, 1.3, 2.2, 2.2, 4.9, 6.3, 6.3, 6.3, 13.2};
-  // const std::vector<double> control_points{0.1, 1.3, 2.2, 4.9, 13.2};
+  constexpr std::size_t degree{3};
+  const std::vector<double> knots{0.1, 1.3, 2.2, 2.2, 4.9, 6.3, 6.3, 6.3, 13.2};
+  const std::vector<double> control_points{0.1, 1.3, 2.2, 4.9, 13.2};
 
-  bsplinex::knots::Atter<double, bsplinex::Curve::UNIFORM, bsplinex::BoundaryCondition::OPEN> atter{
-  };
-
-  // auto bspline = bsplinex::factory::open_nonuniform(degree, knots, control_points);
+  auto bspline = bsplinex::factory::open_nonuniform(degree, knots, control_points);
 
   // evaluate the curve at some points
-  // std::vector<double> eval_x{3.0, 3.4, 5.1, 6.2};
-  // for (const double x : eval_x)
-  // {
-  //   std::cout << "bspline.evaluate(" << x << ") = " << bspline.evaluate(x) << std::endl;
-  // }
+  std::vector<double> eval_x{3.0, 3.4, 5.1, 6.2};
+  for (const double x : eval_x)
+  {
+    std::cout << "bspline.evaluate(" << x << ") = " << bspline.evaluate(x) << std::endl;
+  }
 }
