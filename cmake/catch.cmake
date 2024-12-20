d6858b26c8262ee99676aee13fac333c32f960af
Gabriele Masina <gabriele.masina@virgilio.it>
2024-12-20 10:44:44 +0100

    Now find_package should discover Catch2 in third-party dir

--- cmake/catch.cmake
+++ cmake/catch.cmake
@@ -1,28 +1,47 @@
-include(FetchContent)
-
-set(FETCHCONTENT_BASE_DIR "${THIRD_PARTY_DIR}")
 set(CATCH2_REQUIRED_VERSION 3.7.1)
+cmake_policy(SET CMP0135 NEW)
 
+if(TARGET catchlib)
+  return()
+endif()
 
 add_library(catchlib INTERFACE)
 
 
+set(FETCHCONTENT_BASE_DIR "${BSPLINEX_THIRD_PARTY_DIR}")
 find_package(
   Catch2
   ${CATCH2_REQUIRED_VERSION}
   NO_MODULE
+  QUIET
 )
 
-if(NOT TARGET Catch2::Catch2WithMain)
-  message("Did not find Catch2 ${CATCH2_REQUIRED_VERSION} installed on the system, downloading to ${THIRD_PARTY_DIR}")
+if(NOT TARGET Catch2::Catch2)
+  # We have to do this manually since the install process of
+  # Catch2 does not make the target directly discoverable by find_package
+  if(EXISTS "${FETCHCONTENT_BASE_DIR}/catch2-src")
+    message("Found Catch2 installed in ${FETCHCONTENT_BASE_DIR}/catch2-src")
+    add_subdirectory("${FETCHCONTENT_BASE_DIR}/catch2-src"
+                     "${FETCHCONTENT_BASE_DIR}/catch2-build")
+  else()
+    message("Did not find Catch2 ${CATCH2_REQUIRED_VERSION} installed, "
+            "downloading to ${FETCHCONTENT_BASE_DIR}")
+    include(FetchContent)
 
-  fetchcontent_declare(
+    fetchcontent_declare(
       Catch2
       URL https://github.com/catchorg/Catch2/archive/refs/tags/v${CATCH2_REQUIRED_VERSION}.tar.gz
-  )
+    )
 
-  fetchcontent_makeavailable(Catch2)
+    fetchcontent_makeavailable(Catch2)
+  endif()
+else()
+  get_target_property(CATCH2_INCLUDE_DIRS Catch2::Catch2 INTERFACE_INCLUDE_DIRECTORIES)
+  message("Found Catch2 installed in ${Catch2_INCLUDE_DIRS}")
 endif()
 
-target_link_libraries(catchlib INTERFACE Catch2::Catch2WithMain)
+list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/extras)
+include(CTest)
+include(Catch)
 
+target_link_libraries(catchlib INTERFACE Catch2::Catch2WithMain)
--- cmake/eigen.cmake
+++ cmake/eigen.cmake
@@ -1,20 +1,24 @@
-include(FetchContent)
-
-set(FETCHCONTENT_BASE_DIR "${THIRD_PARTY_DIR}")
 set(EIGEN_REQUIRED_VERSION 3.4)
+cmake_policy(SET CMP0135 NEW)
 
+if(TARGET eigenlib)
+  return()
+endif()
 
 add_library(eigenlib INTERFACE)
 target_compile_features(eigenlib INTERFACE cxx_std_17)
 
+set(FETCHCONTENT_BASE_DIR "${BSPLINEX_THIRD_PARTY_DIR}")
 find_package(
   Eigen3
   ${EIGEN_REQUIRED_VERSION}
   NO_MODULE
+  QUIET
 )
 
 if(NOT TARGET Eigen3::Eigen)
-  message("Did not find Eigen ${EIGEN_REQUIRED_VERSION} installed on the system, downloading to ${THIRD_PARTY_DIR}")
+  message("Did not find Eigen ${EIGEN_REQUIRED_VERSION} installed, downloading to ${BSPLINEX_THIRD_PARTY_DIR}")
+  include(FetchContent)
 
   fetchcontent_declare(
       Eigen3
@@ -22,6 +26,9 @@ if(NOT TARGET Eigen3::Eigen)
   )
 
   fetchcontent_makeavailable(Eigen3)
+else()
+  get_target_property(EIGEN_INCLUDE_DIRS Eigen3::Eigen INTERFACE_INCLUDE_DIRECTORIES)
+  message("Found Eigen installed in ${EIGEN_INCLUDE_DIRS}")
 endif()
 
 target_link_libraries(eigenlib INTERFACE Eigen3::Eigen)
